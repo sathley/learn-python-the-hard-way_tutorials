396b10d831f32fc086fafa311d628d81ffd1b9a8
sathley <sathley@appacitive.com>
2013-10-30 18:02:00 +0530

    added exercises around files and functions

---
+++ lpthw_tuts/src/17. files3.py
@@ -0,0 +1,26 @@
+__author__ = 'sushant'
+
+from sys import argv
+from os.path import exists
+
+from_file = raw_input('Enter from file name :')
+to_file = raw_input('Enter to file name :')
+print "Copying from %s to %s" % (from_file, to_file)
+
+# we could do these two on one line too, how?
+in_file = open(from_file)
+indata = in_file.read()
+
+print "The input file is %d bytes long" % len(indata)
+
+print "Does the output file exist? %r" % exists(to_file)
+print "Ready, hit RETURN to continue, CTRL-C to abort."
+raw_input()
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print "Alright, all done."
+
+out_file.close()
+in_file.close()
